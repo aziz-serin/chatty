4492a85605a629a3675d809df310bf3ad0f1e0d4
Aziz Serin <azizserin313@yahoo.com>
2023-04-23 17:44:56 +0100

    Fix up init for controllers

--- va/controllers/__init__.py
+++ va/controllers/__init__.py
@@ -3,5 +3,7 @@ import flask
 
 mongo_host = flask.current_app.config["mongo"]["host"]
 mongo_port = flask.current_app.config["mongo"]["port"]
+username = flask.current_app.config["mongo"]["username"]
+password = flask.current_app.config["mongo"]["password"]
 
-factory = ConnectionFactory(mongo_host, mongo_port)
+factory = ConnectionFactory(mongo_host, mongo_port, username, password)
