d69e583995fd87f9c3984abcb88d21fd1e31154e
Aziz Serin <azizserin313@yahoo.com>
2023-04-24 20:25:43 +0100

    Change va

---
+++ va.py
@@ -0,0 +1,9 @@
+from va.flaskr import init, register, app
+
+def main():
+    init()
+    register()
+    app.run(app.config["flask"]["host"], app.config["flask"]["port"])
+
+if __name__ == "__main__":
+    main()
