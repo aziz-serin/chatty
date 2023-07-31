689f5f3bcdb1501f8c86a1c2eb84a19e2eb4da15
Aziz Serin <azizserin313@yahoo.com>
2023-07-31 18:34:31 +0300

    Create very simple error page

---
+++ src/ui/src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { fade } from 'svelte/transition';
+	import { afterUpdate } from "svelte";
+</script>
+
+<slot>
+  <h1 transition:fade|global={{delay:100}}>
+    {$page.status} {$page.error.message}
+  </h1>
+</slot>
+
+
+<style>
+  h1 {
+      font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+      text-decoration: #424245;
+      color: #424245;
+      position: absolute;
+      top: 40%;
+      bottom: 40%;
+      text-align: center;
+      width: 100%;
+      height: 100%;
+  }
+</style>
